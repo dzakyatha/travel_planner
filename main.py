3da2e15f9dd5e130db27b99aa0c592a74ae4aeee
dzakyatha <18223124@std.stei.itb.ac.id>
2025-11-14 21:25:22 +0700

    feat: enhance FastAPI configuration, include router, and update root endpoint in main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
+import router
 
-app = FastAPI()
+app = FastAPI(
+    title="API Perencanaan Perjalanan",
+    description="Implementasi API awal Desain Taktis Perencanaan Perjalanan",
+    version="0.1.0"
+)
 
-@app.get("/")
-def root():
-    return {"message": "Hello, World!"}
+app.include_router(router.router, prefix="/api")
+
+@app.get("/", tags=["Root"])
+async def root():
+    return {"message": "Selamat datang di API Perencanaan Perjalanan"}
 
 def main():
     uvicorn.run(app, host="127.0.0.1", port=8000)
