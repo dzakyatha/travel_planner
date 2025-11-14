cdea50e875f6bee717f3558ac0beb68ded93a21c
dzakyatha <18223124@std.stei.itb.ac.id>
2025-11-14 10:56:36 +0700

    feat: Implement FastAPI in main.py and update dependencies in pyproject.toml & uv.lock

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from travel-planner!")
+from fastapi import FastAPI
+import uvicorn
 
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello, World!"}
+
+def main():
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 if __name__ == "__main__":
     main()
